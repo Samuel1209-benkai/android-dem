7ccfc21cfd01d114c72b9b15680f30a457ff5185
samuel SENOUVO <samulsenouvo@gmail.com>
2021-11-29 00:32:48 +0100

    (creation de l'activie calculatrice activity)

---
+++ app/src/main/java/samuelbenkai1209/demo/calclatrice_activity.java
@@ -0,0 +1,14 @@
+package samuelbenkai1209.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calclatrice_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calclatrice_activity);
+    }
+}
